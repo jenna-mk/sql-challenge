644bd31fc899a1a166dbe872fee17e6a18fb967c
Jenna <jennamkutz@gmail.com>
2023-11-16 14:55:40 -0700

    Deleted db and recreated schemas to correctly import data; completed queries 1-8

--- employee_db_schemas.sql
+++ employee_db_schemas.sql
@@ -1,31 +1,22 @@
--- Data Engineering 
+-- Data Engineering --
 
--- Drop tables if they exist
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
+-- Drop tables if they exist already 
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
 
--- Create the needed tables 
+-- Departments table 
 CREATE TABLE departments (
-	dept_no VARCHAR,
+	dept_no VARCHAR PRIMARY KEY,
 	dept_name VARCHAR
 );
 
-CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR
-);
-
-CREATE TABLE dept_manager(
-	dept_no VARCHAR,
-	emp_no INT
-);
-
+-- Create employees table
 CREATE TABLE employees(
-	emp_no INT,
+	emp_no INT PRIMARY KEY,
 	emp_title VARCHAR,
 	birth_date DATE,
 	first_name VARCHAR,
@@ -34,6 +25,65 @@ CREATE TABLE employees(
 	hire_date DATE
 );
 
-CREATE TABLE salaries();
+-- Create department manager table
+CREATE TABLE dept_manager(
+	dept_no VARCHAR,
+	emp_no INT
+);
 
-CREATE TABLE titles();
+-- Create department employees table
+CREATE TABLE dept_emp(
+	emp_no INT,
+	dept_no VARCHAR
+);
+
+-- Create salaries table 
+CREATE TABLE salaries(
+	emp_no INT,
+	salary INT
+);
+
+-- Create titles table
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR
+);
+
+-- Add foreign key constraints
+ALTER TABLE dept_manager
+ADD CONSTRAINT dept_manager_dept_no
+FOREIGN KEY (dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager 
+ADD CONSTRAINT fk_dept_manager_emp_no 
+FOREIGN KEY (emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp
+ADD CONSTRAINT fk_dept_emp_emp_no
+FOREIGN KEY (emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp
+ADD CONSTRAINT fk_dept_emp_dept_no
+FOREIGN KEY (dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE salaries
+ADD CONSTRAINT fk_salaries_emp_no
+FOREIGN KEY (emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_employees_emp_title
+FOREIGN KEY (emp_title)
+REFERENCES titles (title_id);
+
+-- Select the data from the created tables  
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
